e5be25748dcfb40c3786af6306a1b9a3df7e3a4a
Ta! <30303477+Clyux@users.noreply.github.com>
2018-08-05 02:01:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function replaceAll(find, replace, str)
         str = str.replace(find, replace);
       }
       return dem = str;
-    }}); 
+    };
 client.on('messageReactionAdd', (reaction, user) => {
     if(reaction.emoji.name === "✅" && user.id != "475317547483267072"){
         accueil.send("-69 " + user);
